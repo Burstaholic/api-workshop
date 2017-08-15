cda0505c49dd1f4328f15b293e8f0491d8a368b9
Travis Odom <travis.a.odom@gmail.com>
2017-08-15 14:01:53 -0400

    formatting

--- 00-workspace/app.js
+++ 00-workspace/app.js
@@ -1,39 +1,39 @@
 // SET UP THE MAP
 
 var mapProjection = new ol.proj.Projection({
-  code: 'EPSG:3857',
-  extent: [-20037508, -20037508, 20037508, 20037508.34]
+    code: 'EPSG:3857',
+    extent: [-20037508, -20037508, 20037508, 20037508.34]
 })
 var geoProjection = new ol.proj.Projection({
-  code: 'EPSG:4326',
-  extent: [-180, -180, 180, 180]
+    code: 'EPSG:4326',
+    extent: [-180, -180, 180, 180]
 })
 
 var map = new ol.Map({
-  layers:[
-    new ol.layer.Tile({
-      source: new ol.source.XYZ({
-        url: 'https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'
-      })
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.XYZ({
+                url: 'https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'
+            })
+        })
+    ],
+    target: 'map',
+    view: new ol.View({
+        center: ol.proj.transform([-96, 39], geoProjection, mapProjection),
+        zoom: 5
     })
-  ],
-  target: 'map',
-  view: new ol.View({
-    center: ol.proj.transform([-96, 39], geoProjection, mapProjection),
-    zoom: 5
-  })
 });
 
 let app = {
-	mapzenKey: 'mapzen-moAoHZd',
-  activeSearch: 'from',
-  
-  typeAhead: function(e)
-  {
-    let el = e.target;
-    let val = el.value;
-    console.log('Got:', val);
-  }
+    mapzenKey: 'mapzen-moAoHZd',
+    activeSearch: 'from',
+
+    typeAhead: function (e)
+    {
+        let el = e.target;
+        let val = el.value;
+        console.log('Got:', val);
+    }
 };
 
-$('#search-from-input').on('keyup', {input: 'from'}, app.typeAhead)
+$('#search-from-input').on('keyup', {input: 'from'}, app.typeAhead);
